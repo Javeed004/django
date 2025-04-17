1a13424af7507de690449c5041e7c20eddbfc6c4
Javeed004 <javeedahamed1404@gmail.com>
2025-04-17 12:22:07 +0530

    Added Files

--- myproject/kct/models.py
+++ myproject/kct/models.py
@@ -8,10 +8,4 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    city = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
